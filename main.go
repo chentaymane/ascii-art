65ed30e3c9a6e7a92986984d3e23cc7895f5a988
chentaymane <chentaymane234@gmail.com>
2025-11-30 23:11:31 +0100

    gg

--- main.go
+++ main.go
@@ -6,22 +6,51 @@ import (
 	"strings"
 )
 
-func main(){
+func main() {
 	if len(os.Args) != 2 {
-		fmt.Println("eror number of inputs non valide ")
+		fmt.Println("error: invalid number of inputs")
 		return
 	}
-//	input:= os.Args[1]
-font := "standard.txt"
-content ,err := os.ReadFile(font)
-if err != nil {
-	fmt.Println(err)
-	return
-}
-lines := strings.Split(string(content),"\n")
-		fmt.Println(lines)
 
-	//fmt.Println(string('~'-('~'-'!')))
+	input := os.Args[1]
 
+	linesInput := strings.Split(input, "\\n")
 
+	font := "standard.txt"
+	content, err := os.ReadFile(font)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	text := strings.ReplaceAll(string(content), "\r\n", "\n")
+	fontLines := strings.Split(text, "\n")
+
+	for _, line := range linesInput {
+
+		if line == "" {
+			fmt.Println()
+			continue
+		}
+		runes := []rune(line)
+		chars := make([][]string, len(runes))
+
+		for i, char := range runes {
+			if char < ' ' || char > '~' {
+				fmt.Println("unsupported character")
+				return
+			}
+
+			index := int(((char - ' ') * 9) + 1)
+			chars[i] = fontLines[index : index+8]
+		}
+
+		for height := 0; height < 8; height++ {
+			for i := range chars {
+				fmt.Print(chars[i][height])
+			}
+			fmt.Println()
+		}
+
+	}
 }
